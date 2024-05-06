f8e3a928dc83e31a3ecd47254ef73798dcc5de2a
vishwas-patil <vishpatil145@gmail.com>
2024-05-06 14:24:04 +0530

    Complete the assignment with all requirements

--- backend-live-coding-exercise-ruby-main/questionnaire.rb
+++ backend-live-coding-exercise-ruby-main/questionnaire.rb
@@ -1,7 +1,7 @@
-require "pstore" # https://github.com/ruby/pstore
+require "pstore"
+require 'byebug'
 
 STORE_NAME = "tendable.pstore"
-store = PStore.new(STORE_NAME)
 
 QUESTIONS = {
   "q1" => "Can you code in Ruby?",
@@ -11,18 +11,50 @@ QUESTIONS = {
   "q5" => "Can you code in C#?"
 }.freeze
 
-# TODO: FULLY IMPLEMENT
 def do_prompt
-  # Ask each question and get an answer from the user's input.
-  QUESTIONS.each_key do |question_key|
-    print QUESTIONS[question_key]
-    ans = gets.chomp
+  answers = {} # Store answers for each and every question
+  QUESTIONS.each do |question_key, question|
+    print "#{question} (Yes/No): "
+    answer = gets.chomp.downcase
+    # Ensure the answer is either "yes" or "no"
+    until ["yes", "no", "y", "n"].include?(answer)
+      print "Please enter Yes or No: "
+      answer = gets.chomp.downcase
+    end
+    answers[question_key] = answer
+  end
+  persist_answers(answers)
+  calculate_rating(answers)
+end
+
+def persist_answers(answers)
+  store = PStore.new(STORE_NAME)
+  store.transaction do
+    store[:answers] ||= []
+    store[:answers] << answers
   end
 end
 
-def do_report
-  # TODO: IMPLEMENT
+def calculate_rating(answers)
+  yes_count = answers.count { |_, answer| ["yes", "y"].include?(answer) }
+  rating = (yes_count.to_f / QUESTIONS.size) * 100
+  puts "Your rating for this run: #{rating.round(2)}%"
+  rating
+end
+
+def calculate_average_rating
+  store = PStore.new(STORE_NAME)
+  total_rating = 0
+  total_runs = 0
+  store.transaction do
+    store[:answers]&.each do |answers|
+      total_rating += calculate_rating(answers)
+      total_runs += 1
+    end
+  end
+  average_rating = total_rating / total_runs unless total_runs.zero?
+  puts "Average rating across all runs: #{average_rating.round(2)}%" if average_rating
 end
 
 do_prompt
-do_report
+calculate_average_rating
